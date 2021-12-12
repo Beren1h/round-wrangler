71bcd965ee31e5126497057e8111604b39488ea3
Beren1h <mobile9921@gmail.com>
2021-12-12 14:35:37 -0500

    new dials

--- src/Handlers/ControlHandler.cs
+++ src/Handlers/ControlHandler.cs
@@ -14,20 +14,25 @@ namespace wrangler.handlers
         private readonly MemoryBank _bank;
         private readonly CombatantHandler _combatant;
         private readonly CombatHandler _combat;
+        private readonly AffectHandler _affect;
 
 
         public ControlHandler(
             MemoryBank bank,
             CombatantHandler combatant,
             CombatHandler combat,
+            AffectHandler affect,
             IOptions<Party> options
         ){
             _bank = bank;
             _combatant = combatant;
             _combat = combat;
+            _affect = affect;
         }
 
         public string AddedCombatantName {get; set; }
+        //public string AddedAffectName { get; set; }
+        //public Affect AddedAffect { get; set; }
 
         public List<WidgetProperties> GetAffectWidgets(Combatant combatant)
         {
@@ -57,54 +62,54 @@ namespace wrangler.handlers
             }
         }
 
-        public string DetermineAffectCssClass(Affect affect)
-        {
-            if (_bank.AffectBeingAssigned == affect)
-            {
-                return "assignment";
-            }
+        // public string DetermineAffectCssClass(Affect affect)
+        // {
+        //     if (_bank.AffectBeingAssigned == affect)
+        //     {
+        //         return "assignment";
+        //     }
 
-            return string.Empty;
-        }
+        //     return string.Empty;
+        // }
 
-        public string DetermineConcentrationCssClass()
-        {
-            var combatant = _bank.Combat.Turn;
+        // public string DetermineConcentrationCssClass()
+        // {
+        //     var combatant = _bank.Combat.Turn;
 
-            if (_bank.Combat.Round == 0 || combatant == null)
-            {
-                return string.Empty;
-            }
+        //     if (_bank.Combat.Round == 0 || combatant == null)
+        //     {
+        //         return string.Empty;
+        //     }
 
-            foreach(var affect in _bank.Affects)
-            {
-                if (affect.IsConcentration == resources.Affects.Concentration.YES && affect.Expiration.Turn == combatant.Name)
-                {
-                    return "concentration";
-                }
-            }
+        //     foreach(var affect in _bank.Affects)
+        //     {
+        //         if (affect.IsConcentration == resources.Affects.Concentration.YES && affect.Expiration.Turn == combatant.Name)
+        //         {
+        //             return "concentration";
+        //         }
+        //     }
 
-            return string.Empty;
-        }
+        //     return string.Empty;
+        // }
 
-        public string DetermineCombatantWidgetCssClass(Combatant combatant)
-        {
-            var css = string.Empty;
+        // public string DetermineCombatantWidgetCssClass(Combatant combatant)
+        // {
+        //     var css = string.Empty;
 
-            if (!combatant.IsActive || combatant.IsTurn || combatant.TurnTaken)
-            {
-                css += " muted";
-            }
+        //     if (!combatant.IsActive || combatant.IsTurn || combatant.TurnTaken)
+        //     {
+        //         css += " muted";
+        //     }
 
-            if (combatant.IsActive && 
-                _bank.Affects.Any(a => a.IsConcentration == resources.Affects.Concentration.YES && 
-                a.Expiration.Turn == combatant.Name))
-            {
-                css += " concentration";
-            }
+        //     if (combatant.IsActive && 
+        //         _bank.Affects.Any(a => a.IsConcentration == resources.Affects.Concentration.YES && 
+        //         a.Expiration.Turn == combatant.Name))
+        //     {
+        //         css += " concentration";
+        //     }
 
-            return css;
-        }
+        //     return css;
+        // }
 
         public void OnEnter(KeyboardEventArgs e)
         {
