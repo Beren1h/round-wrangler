667e27b0562fe65ae9f6645ddb43659664cbc42d
Beren1h <mobile9921@gmail.com>
2021-12-24 15:51:53 -0500

    better upload; bools are bools

--- src/Models/Affect.cs
+++ src/Models/Affect.cs
@@ -8,23 +8,20 @@ namespace wrangler.models
         public Affect()
         {
             _id = Guid.NewGuid().ToString();
-            IsConcentration = resources.Affects.Concentration.NO;
-            Expiration = new Expiration{
-                Pointer = resources.Affects.Pointers.END
-            };
+            Expiration = new Expiration();
         }
 
         public string Id { get { return _id; } }
         public string Description { get; set; }
         public bool AssignmentMode { get; set; }
         public Expiration Expiration { get; set; }
-        public string IsConcentration { get; set; }
+        public bool IsConcentration { get; set; }
     }
 
     public class Expiration
     {
         public int Round { get; set; }
         public string Turn { get; set; }
-        public string Pointer { get; set; }
+        public bool AtStart { get; set; }
     }
 }
