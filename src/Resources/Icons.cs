5411e0ebd4708acff8a88ff4782d47f68a0718ff
Beren1h <mobile9921@gmail.com>
2021-12-25 06:54:44 -0500

    icons as list

--- src/Resources/Icons.cs
+++ src/Resources/Icons.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace wrangler.resources
 {
     public static class Icons
     {
+        public static List<string> List = new List<string> {
+            Icons.SKULL,
+            Icons.FLED,
+            Icons.QUESTION
+        };
+        
         public const string SKULL = "skull";
         public const string FLED = "fled";
         public const string QUESTION = "question";
